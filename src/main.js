60cdd4b6b72ab661e09c0dec058c2fcf4be91f61
chenliwen123 <1410762621@qq.com>
2021-07-09 00:21:16 +0800

    引入代理

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import router from './router/index'
 import store from './store/index'
+import axios from 'axios';
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
+app.config.globalProperties.$axios=axios;
 app.mount('#app')
