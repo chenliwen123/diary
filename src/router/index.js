3810312f4ebc1640156ecb039ef63c1deac3f42d
chenliwen123 <1410762621@qq.com>
2021-08-06 09:56:09 +0800

    全局路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, onBeforeRouteLeave } from 'vue-router'
 import indexview from '../view/index.vue'
 import diary from '../view/diary.vue'
 import exampleRoutes from './example'
@@ -12,6 +12,12 @@ const routes = [
   const router = createRouter({
     history: createWebHashHistory(),
     routes:[...exampleRoutes,...routes], 
+  })
+
+  router.beforeEach( (to,from) => {
+    console.log(from) // 从哪里来
+    console.log(to) // 到哪里去
+    // next() 可以不写 return false 就是停止执行
   })
 
   export default router
